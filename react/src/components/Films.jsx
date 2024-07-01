36b9a939c63aeb2ef0d89851599ba2cc10be0c48
Arvind Kasiliya <arvind@kasiliya.com>
2024-07-01 16:25:12 -0400

    Integrate server with characters page

--- react/src/components/Films.jsx
+++ react/src/components/Films.jsx
@@ -1,26 +1,30 @@
 const Films = (film) => {
-    return (
-        <body>
-        <main>
-          <h1 id="name">{film.name}</h1>
-          <section id="generalInfo">
-            <p>Released: <span id="released">{film.released}</span></p>
-            <p>Director: <span id="director">{film.director}</span></p>
-            <p>Episode: <span id="episode">{film.episode}</span></p>
-          </section>
-          <section id="characters">
-            <h2>Characters in Film</h2>
-            <ul></ul>
-          </section>
-          <section id="planets">
-            <h2>Planets in Film</h2>
-            <ul></ul>
-          </section>
-          
-        </main>
-      </body>
-
-    );
+  return (
+    <body>
+      <main>
+        <h1 id="name">{film.title}</h1>
+        <section id="generalInfo">
+          <p>
+            Released: <span id="released">{film.release_date}</span>
+          </p>
+          <p>
+            Director: <span id="director">{film.director}</span>
+          </p>
+          <p>
+            Episode: <span id="episode">{film.episode_id}</span>
+          </p>
+        </section>
+        <section id="characters">
+          <h2>Characters in Film</h2>
+          <ul></ul>
+        </section>
+        <section id="planets">
+          <h2>Planets in Film</h2>
+          <ul></ul>
+        </section>
+      </main>
+    </body>
+  );
 };
 
 export default Films;
