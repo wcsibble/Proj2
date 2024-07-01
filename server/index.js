2b3da0aa4584f052a7eb4b6fede2fe21d8edf329
Arvind Kasiliya <arvind@kasiliya.com>
2024-07-01 13:21:20 -0400

    Use star wars data in server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import cors from "cors";
 dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
-const collectionName = process.env.MONGO_DB_COLLECTION;
+
+const client = await MongoClient.connect(url);
+const db = client.db(dbName);
 
 const app = express();
 app.use(cors());
@@ -17,7 +19,69 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/api/planets", async (req, res) => {
-  res.json({ hello: "world" });
+  const collection = db.collection("planets");
+  const planets = await collection.find({}).toArray();
+  res.json(planets);
+});
+
+app.get("/api/characters", async (req, res) => {
+  const collection = db.collection("characters");
+  const data = await collection.find({}).toArray();
+  res.json(data);
+});
+
+app.get("/api/films", async (req, res) => {
+  const collection = db.collection("films");
+  const data = await collection.find({}).toArray();
+  res.json(data);
+});
+
+app.get("/api/planets/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+
+    const collection = db.collection("planets");
+    const findRes = await collection.findOne({ id: Number(id) });
+    res.json(findRes);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Hmm, something doesn't smell right... Error deleting sock");
+  }
+});
+
+app.get("/api/characters/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+
+    const collection = db.collection("characters");
+    const findRes = await collection.findOne({ id: Number(id) });
+    res.json(findRes);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Hmm, something doesn't smell right... Error deleting sock");
+  }
+});
+
+app.get("/api/films/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+
+    const collection = db.collection("films");
+    const findRes = await collection.findOne({ id: Number(id) });
+    res.json(findRes);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Hmm, something doesn't smell right... Error deleting sock");
+  }
 });
 
 app.listen(PORT, () => {
